0c9cad93246cac195a356b77157523b69516620f
BiserBorisov <butoff@abv.bg>
2018-02-25 15:24:33 +0200

    DB exercises

---
+++ CSharpDB/DB_Advanced_02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB_Advanced_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var acc = new BankAccount();
+            acc.ID = 1;
+            acc.Deposit(15);
+            acc.Withdraw(5);
+
+            Console.WriteLine($"Account {acc.ID}, balance = {acc.Balance:f2}.");
+        }
+    }
+}
