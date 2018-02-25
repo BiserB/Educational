23254df2709901ea5eb39583925569008d73acce
BiserBorisov <butoff@abv.bg>
2018-02-25 15:22:55 +0200

    DB exercises

---
+++ CSharpDB/DB_Advanced_01/BankAccount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB_Advanced_01
+{
+    class BankAccount
+    {
+        public int ID { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
---
+++ CSharpDB/DB_Advanced_01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB_Advanced_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount acc = new BankAccount();
+            acc.ID = 1;
+            acc.Balance = 15;
+            Console.WriteLine($"Account {acc.ID}, balance {acc.Balance}");
+        }
+    }
+    
+}
