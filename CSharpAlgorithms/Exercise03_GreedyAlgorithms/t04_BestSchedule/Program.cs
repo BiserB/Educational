ac4684de7573c97a890c1dd2a0152f0b24e5d200
BiserBorisov <butoff@abv.bg>
2018-04-04 12:03:28 +0300

    CSharp Grredy_Algorithms

---
+++ CSharpAlgorithms/Exercise03_GreedyAlgorithms/t04_BestSchedule/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine()
+                .Split(new string[] { ": " }, StringSplitOptions.RemoveEmptyEntries);
+        int count = int.Parse(input[1]);
+        List<Lecture> lectures = new List<Lecture>(count);
+        List<Lecture> schedule = new List<Lecture>();
+
+        for (int i = 0; i < count; i++)
+        {
+            string[] args = Console.ReadLine()
+                .Split(new string[] { ": ", " - " }, StringSplitOptions.RemoveEmptyEntries);
+            string name = args[0];
+            int start = int.Parse(args[1]);
+            int end = int.Parse(args[2]);
+
+            Lecture lecture = new Lecture(name, start, end);
+
+            lectures.Add(lecture);
+        }
+
+        while (lectures.Count > 0)
+        {
+            int earliest = lectures.Min(l => l.EndHour);
+
+            Lecture chosenLecture = lectures.First(l => l.EndHour == earliest);
+
+            lectures.Remove(chosenLecture);
+
+            schedule.Add(chosenLecture);
+
+            for (int i = 0; i < lectures.Count; i++)
+            {
+                if (lectures[i].StartHour < chosenLecture.EndHour)
+                {
+                    lectures.Remove(lectures[i]);
+                    i--;
+                }
+            }
+        }
+
+        Console.WriteLine($"Lectures ({schedule.Count}):");
+
+        foreach (var l in schedule)
+        {
+            Console.WriteLine($"{l.StartHour}-{l.EndHour} -> {l.Name}");
+        }
+
+    }
+}
+
+public class Lecture
+{
+    public Lecture(string name, int start, int end)
+    {
+        Name = name;
+        StartHour = start;
+        EndHour = end;
+    }
+    public string Name { get; }
+    public int StartHour { get; }
+    public int EndHour { get; }
+}
---
+++ CSharpAlgorithms/Exercise03_GreedyAlgorithms/t05_EgyptianFractions/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+
+public class Program
+{
+    static void Main()
+    {
+        string[] data = Console.ReadLine().Split('/');
+
+        int p = int.Parse(data[0]);
+        int q = int.Parse(data[1]);
+
+        PrintEgyptianFractions(p, q);
+    }
+
+    private static void PrintEgyptianFractions(int p, int q)
+    {
+        // If either numerator or denominator is 0
+        if (p == 0 || q == 0)
+        {
+            return;
+        }
+
+        // If numerator divides denominator, then simple division
+        // makes the fraction in 1/n form
+        if (p % q == 0)
+        {
+            Console.Write($" 1/{p/q}");
+            return;
+        }
+
+        // If denominator divides numerator, then the given number
+        // is not fraction
+        if (q % p == 0)
+        {
+            Console.Write($" {p/q}");
+            return;
+        }
+
+        // If numerator is more than denominator
+        if (p > q)
+        {
+            Console.Write($" {p / q}");
+            PrintEgyptianFractions(p % q, q);
+            return;
+        }
+
+        // We reach here q > p and q % p is non-zero
+        // Find ceiling of q/p and print it as first
+        // fraction
+
+        int n = q / p + 1;
+        Console.Write($" 1/{n}");
+
+        PrintEgyptianFractions(p * n - q, q * n);
+    }
+}
