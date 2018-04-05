822c4e40dbc5d59f536e9b2068c97b84fce1f9de
BiserBorisov <butoff@abv.bg>
2018-04-05 18:03:21 +0300

    C# OOP Advanced Unit Testing Lab

---
+++ CSharpOOP_Advanced/Lab05_UnitTesting/SceletonTests/DummyTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SceletonTests
+{
+    public class DummyTests
+    {
+        private const int initialHealth = 10;
+        private const int initialExperience = 10;
+        
+        
+        private Dummy CreateDummy()
+        {
+            return new Dummy(initialHealth, initialExperience);
+        }
+
+        [Test]
+        public void DummyLosesHealthIfAttacked()
+        {
+            var dummy = CreateDummy();
+
+            dummy.TakeAttack(1);
+
+            Assert.That(dummy.Health, Is.EqualTo(9), "Dummy's health is not affected");
+        }
+
+        [Test]
+        public void DeadDummyThrowsExceptionIfAttacked()
+        {
+            var dummy = CreateDummy();
+
+            dummy.TakeAttack(10);
+
+            Assert.Throws<InvalidOperationException>(() => dummy.TakeAttack(10));
+        }
+
+        [Test]
+        public void DeadDummyCanGiveXP()
+        {
+            var dummy = CreateDummy();
+
+            Assert.Throws<InvalidOperationException>(() => dummy.GiveExperience());
+        }
+
+        
+    }
+}
---
+++ CSharpOOP_Advanced/Lab05_UnitTesting/SceletonTests/HeroTests.cs
@@ -0,0 +1,46 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SceletonTests
+{
+    public class HeroTests
+    {
+        private const int targetExperience = 5;
+        private const int targetHealth = 5;
+        private const int axeAttack = 10;
+        private const int axeDurability = 10;
+
+        [Test]
+        public void HeroGainsXPFromTarget()
+        {
+            IWeapon axe = new Axe(axeAttack, axeDurability);
+
+            ITarget dummy = new Dummy(targetHealth, targetExperience);
+
+            Hero testHero = new Hero("SuperHero", axe);
+
+            testHero.Attack(dummy);
+
+            Assert.That(testHero.Experience, Is.EqualTo(targetExperience));
+        }
+
+        [Test]
+        public void MockTest_HeroGainsXPFromTarget()
+        {
+            var target = new Mock<ITarget>();
+            target.Setup(t => t.IsDead()).Returns(true);
+            target.Setup(t => t.GiveExperience()).Returns(targetExperience);
+            var weapon = new Mock<IWeapon>();
+            Hero testHero = new Hero("SuperHero", weapon.Object);
+
+            testHero.Attack(target.Object);
+
+            Assert.That(testHero.Experience, Is.EqualTo(targetExperience));
+        }
+
+
+    }
+}
