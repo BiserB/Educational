d7b92e44a895d5bc853f180e286938641e296833
BiserBorisov <butoff@abv.bg>
2018-05-01 11:22:19 +0300

    OOP Advanced Attributes

---
+++ CSharpOOP_Advanced/AttributesTesting/AttributesTesting/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+
+namespace AttributesTesting
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Type[] types = Assembly.GetExecutingAssembly().GetTypes();
+
+            foreach (var type in types)
+            {
+                Console.WriteLine(type.Name);
+                Console.WriteLine("  -Attributes:");
+                foreach (var attr in type.GetCustomAttributes())
+                {
+                    Console.WriteLine(attr.GetType().Name);
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+    }
+
+    [TestAttribute]
+    class MyTestSuite
+    {
+
+    }
+
+    class TestAttribute : Attribute
+    {
+
+    }
+}
